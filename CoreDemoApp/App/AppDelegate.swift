ddfbff30a8f4a0c22066fd44e7acbc9c29509a15
maximsamus <maximsamus1@gmail.com>
2022-04-28 18:38:53 +0300

    Logic Update

--- CoreDemoApp/App/AppDelegate.swift
+++ CoreDemoApp/App/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -30,30 +28,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         StorageManager.shared.saveContext()
     }
-
-//    // MARK: - Core Data stack
-//    lazy var persistentContainer: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: "CoreDemoApp")
-//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        })
-//        return container
-//    }()
-//
-//    // MARK: - Core Data Saving support
-//    func saveContext() {
-//        let context = persistentContainer.viewContext
-//        if context.hasChanges {
-//            do {
-//                try context.save()
-//            } catch {
-//                let nserror = error as NSError
-//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-//            }
-//        }
-//    }
-
 }
 
--- CoreDemoApp/ViewControllers/TaskListViewController.swift
+++ CoreDemoApp/ViewControllers/TaskListViewController.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
-//import CoreData
 
 class TaskListViewController: UITableViewController {
     
     
     private var taskList: [Task] = []
     private let cellID = "task"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
@@ -21,7 +20,7 @@ class TaskListViewController: UITableViewController {
         setupNavigationBar()
         fetchData()
     }
-
+    
     private func setupNavigationBar() {
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -49,7 +48,7 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addNewTask() {
-        showAlert(with: "New Task", and: "What do you want to do?")
+        showAlert()
     }
     
     private func fetchData() {
@@ -61,25 +60,20 @@ class TaskListViewController: UITableViewController {
                 print(error.localizedDescription)
             }
         }
-//        let fetchRequest = Task.fetchRequest()
-//        do {
-//            taskList = try viewContext.fetch(fetchRequest)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
     }
     
-    private func showAlert(with title: String, and message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            self.save(task)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        alert.addTextField { textField in
-            textField.placeholder = "New Task"
+    
+    private func showAlert(task: Task? = nil, completion: (() -> Void)? = nil) {
+        let title = task != nil ? "Update Task" : "New Task"
+        let alert = UIAlertController.createAlertController(withTitle: title)
+        
+        alert.action(task: task) { taskName in
+            if let task = task, let completion = completion {
+                StorageManager.shared.update(task, newName: taskName)
+                completion()
+            } else {
+                self.save(taskName)
+            }
         }
         present(alert, animated: true)
     }
@@ -92,21 +86,6 @@ class TaskListViewController: UITableViewController {
                 with: .automatic
             )
         }
-        
-//        let task = Task(context: viewContext)
-//        task.title = taskName
-//        taskList.append(task)
-//
-//        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
-//        tableView.insertRows(at: [cellIndex], with: .automatic)
-//
-//        if viewContext.hasChanges {
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
     }
 }
 
@@ -122,5 +101,21 @@ extension TaskListViewController {
         content.text = task.title
         cell.contentConfiguration = content
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let task = taskList[indexPath.row]
+        showAlert(task: task) {
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let task = taskList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            StorageManager.shared.delete(task)
+        }
     }
 }
